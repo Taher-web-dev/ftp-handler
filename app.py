c2a1a7c564c72c2ae1a38c6633707ecf53c88d54
aziz <aziz@tanitlab.com>
2021-04-19 22:13:37 +0100

    new format changed to asc.

--- app.py
+++ app.py
@@ -152,7 +152,7 @@ def ftp_transfer_job(data, target, filename):
 def all_ported_numbers_transfer_job(target):
     print(f"All ported numbers Job started for {target} ")
     data = dbf.collection('portings').stream()
-    data2 = dbf.collection('portings').order_by("date_porting", direction=Query.DESCENDING).stream()
+    data2 = dbf.collection('portings').order_by("date_porting", direction=Query.ASCENDING).stream()
     data = [d.to_dict() for d in data]
     data2 = [d.to_dict() for d in data2]
     status = True
