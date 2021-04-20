02ee9e91a16f55308348148d39b4b7a2eb9c9cd0
aziz <aziz@tanitlab.com>
2021-04-20 15:48:41 +0100

    bugfix.

--- app.py
+++ app.py
@@ -181,7 +181,7 @@ def all_ported_numbers_transfer_job(target):
             ftp.login(LNP_USER, LNP_PASSWORD)
             # store latest file
             ftp.storbinary(f"STOR {lnp_last_update_dir}/ported_numbers.csv",
-                           df.to_csv(index=False, sep=",", media_type="text/csv"))
+                           df.to_csv(index=False, sep=","))
             # store latest file CELL
             ftp.storbinary(f"STOR {cell_filename}", bio_cell)
             # store history
